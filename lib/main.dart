dcf3e16eb9232087a295b4b4425d17c150b6440b
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-29 11:13:34 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'sign_in_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.light(),
+      home: SignInPage(),
+    );
+  }
+}
---
+++ lib/sign_in_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatefulWidget {
+  const SignInPage({super.key});
+
+  @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _userNameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("To Do"),
+        centerTitle: true,
+        backgroundColor: Colors.black12,
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Form(
+              key: _formKey,
+              child: TextFormField(
+                controller: _userNameController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
