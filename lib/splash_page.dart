122d23eafff815dd1f3868f92d5955ce5bc91a0b
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-30 22:07:29 +0530

    splash_page.dart : class add, heading add, image add, initState add, Future.delayed method call inside init state(),

--- lib/splash_page.dart
+++ lib/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_app/sign_in_page.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
@@ -10,13 +11,22 @@ class SplashPage extends StatefulWidget {
 class _SplashPageState extends State<SplashPage> {
 
   @override
+  // init call at once only when the page load
   void initState() {
     super.initState();
+    debugPrint('InitState method');
+    Future.delayed( const Duration(seconds: 3), () {
+
+    });
   }
+
+
 
 
   @override
   Widget build(BuildContext context) {
+    debugPrint('build method');
+
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
@@ -38,7 +48,10 @@ class _SplashPageState extends State<SplashPage> {
                 style: TextStyle(fontWeight: FontWeight.w900, fontSize: 50),
               ),
               SizedBox(
-                width: MediaQuery.of(context).size.width,
+                width: MediaQuery
+                    .of(context)
+                    .size
+                    .width,
                 child: Text(
                   'by Ratndeep',
                   style: TextStyle(fontSize: 20, fontStyle: FontStyle.italic),
