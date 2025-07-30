9b8892f2d34dffdcbb1168767a4247121181aad3
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-30 21:12:17 +0530

    main.dart : home : SplashPage(),

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'sign_in_page.dart';
+import 'package:to_do_app/splash_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.light(),
-      home: SignInPage(),
+      home: SplashPage(),
     );
   }
 }
