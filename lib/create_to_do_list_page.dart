4d8189b8da2f1df582545853485d964806b0226a
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-01 15:00:15 +0530

    create_to_do_list_page.dart :

---
+++ lib/create_to_do_list_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateToDoListPage extends StatefulWidget {
+  const CreateToDoListPage({super.key});
+
+  @override
+  State<CreateToDoListPage> createState() => _CreateToDoListPageState();
+}
+
+class _CreateToDoListPageState extends State<CreateToDoListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
