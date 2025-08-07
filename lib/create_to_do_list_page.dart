dc7fe7a25753740d55e749255742e3ee288e70ad
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-07 20:36:20 +0530

    create_to_do_list_page.dart : close icon ass, text add, set remainder add, dynamic textField add,

--- lib/create_to_do_list_page.dart
+++ lib/create_to_do_list_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'create to do page widgets/custom_inputfields_widget.dart';
 import 'create to do page widgets/set_remainder_widget.dart';
 
 class CreateToDoListPage extends StatefulWidget {
@@ -42,6 +43,18 @@ class _CreateToDoListPageState extends State<CreateToDoListPage> {
 
               /// Set Remainder
               SetRemainderWidget(),
+
+              SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+              Text(
+                'Tells us about your task',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+
+              /// TextFields
+              CustomTextFieldWidget(hint: 'Title'),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.015),
+              CustomTextFieldWidget(hint: 'Description'),
             ],
           ),
         ),
