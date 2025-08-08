676110ea28b5890e43fec46d43f9456b06aa09f4
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-08 13:14:18 +0530

    create_to_do_list_page.dart : close icon ass, text add, set remainder add, dynamic textField add, repeat section add, days section add,

--- lib/create_to_do_list_page.dart
+++ lib/create_to_do_list_page.dart
@@ -16,88 +16,97 @@ class _CreateToDoListPageState extends State<CreateToDoListPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                /// Close Button
-                InkWell(
-                  onTap: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: Icon(Icons.close, weight: 20.0, size: 25),
-                ),
-                SizedBox(height: MediaQuery.of(context).size.height * 0.03),
-            
-                ///. Title
-                InkWell(
-                  onTap: () {},
-                  child: Text(
-                    'Create to-do',
-                    style: TextStyle(
-                      fontSize: 35,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 1.5,
+      body: InkWell(
+        // Disables the splash effect
+        splashFactory: NoSplash.splashFactory,
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  /// Close Button
+                  InkWell(
+                    splashFactory: NoSplash.splashFactory,
+                    onTap: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Icon(Icons.close, weight: 20.0, size: 25),
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+
+                  ///. Title
+                  InkWell(
+                    splashFactory: NoSplash.splashFactory,
+                    onTap: () {},
+                    child: Text(
+                      'Create to-do',
+                      style: TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1.5,
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(height: MediaQuery.of(context).size.height * 0.02),
-            
-                /// Set Remainder
-                SetRemainderWidget(),
-            
-                SizedBox(height: MediaQuery.of(context).size.height * 0.04),
-            
-                /// Task Description
-                CustomText(text: 'Tells us about your task'),
-                SizedBox(height: MediaQuery.of(context).size.height * 0.01),
-            
-                /// Text Fields
-                CustomTextFieldWidget(hint: 'Title'),
-                SizedBox(height: MediaQuery.of(context).size.height * 0.015),
-                CustomTextFieldWidget(hint: 'Description'),
-            
-                SizedBox(height: MediaQuery.of(context).size.height * 0.04),
-            
-                /// Repeat Section
-                CustomText(text: 'Repeat'),
-                SizedBox(height: MediaQuery.of(context).size.height * 0.01),
-            
-                /// Timeline Section
-                Wrap(
-                  direction: Axis.horizontal,
-                  runSpacing: MediaQuery.of(context).size.height * 0.012,
-                  spacing: MediaQuery.of(context).size.width * 0.03,
-                  children: [
-                    /// Parent Timeline
-                    CustomChip(title: 'Daily'),
-                    CustomChip(title: 'Weekly'),
-                    CustomChip(title: 'Monthly'),
-                    CustomChip(title: 'No repeat'),
-                  ],
-                ),
-            
-                SizedBox(height: MediaQuery.of(context).size.height * 0.04),
-            
-                /// Days Section
-                Wrap(
-                  direction: Axis.horizontal,
-                  runSpacing: MediaQuery.of(context).size.height * 0.012,
-                  spacing: MediaQuery.of(context).size.width * 0.03,
-                  children: [
-                    CustomChip(title: 'Sunday'),
-                    CustomChip(title: 'Monday'),
-                    CustomChip(title: 'Tuesday'),
-                    CustomChip(title: 'Wednesday'),
-                    CustomChip(title: 'Thursday'),
-                    CustomChip(title: 'Friday'),
-                    CustomChip(title: 'Saturday'),
-                  ],
-                ),
-              ],
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+
+                  /// Set Remainder
+                  SetRemainderWidget(),
+
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.04),
+
+                  /// Task Description
+                  CustomText(text: 'Tells us about your task'),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+
+                  /// Text Fields
+                  CustomTextFieldWidget(hint: 'Title'),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.015),
+                  CustomTextFieldWidget(hint: 'Description'),
+
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.04),
+
+                  /// Repeat Section
+                  CustomText(text: 'Repeat'),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+
+                  /// Timeline Section
+                  Wrap(
+                    direction: Axis.horizontal,
+                    runSpacing: MediaQuery.of(context).size.height * 0.012,
+                    spacing: MediaQuery.of(context).size.width * 0.03,
+                    children: [
+                      /// Parent Timeline
+                      CustomChip(title: 'Daily'),
+                      CustomChip(title: 'Weekly'),
+                      CustomChip(title: 'Monthly'),
+                      CustomChip(title: 'No repeat'),
+                    ],
+                  ),
+
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.04),
+
+                  /// Days Section
+                  Wrap(
+                    direction: Axis.horizontal,
+                    runSpacing: MediaQuery.of(context).size.height * 0.012,
+                    spacing: MediaQuery.of(context).size.width * 0.03,
+                    children: [
+                      CustomChip(title: 'Sunday'),
+                      CustomChip(title: 'Monday'),
+                      CustomChip(title: 'Tuesday'),
+                      CustomChip(title: 'Wednesday'),
+                      CustomChip(title: 'Thursday'),
+                      CustomChip(title: 'Friday'),
+                      CustomChip(title: 'Saturday'),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
