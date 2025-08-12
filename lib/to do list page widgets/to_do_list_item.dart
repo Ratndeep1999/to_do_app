f8776c2f8a090365e88536ea04b80e367b1427af
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-12 18:47:33 +0530

    to_do_list_item.dart : blue circle add, title add, description add, date add, time add, notification add, all data dynamically set

--- lib/to do list page widgets/to_do_list_item.dart
+++ lib/to do list page widgets/to_do_list_item.dart
@@ -2,11 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:to_do_app/to_do_model_class.dart';
 
 class ToDoListItem extends StatefulWidget {
-  const ToDoListItem({
-    super.key,
-    required this.index,
-    required this.item,
-  });
+  const ToDoListItem({super.key, required this.index, required this.item});
 
   final int index;
   final ToDoModel item;
@@ -16,21 +12,57 @@ class ToDoListItem extends StatefulWidget {
 }
 
 class _ToDoListItemState extends State<ToDoListItem> {
+
+  // Calculations happen once, not every build() call.
+  // Cleaner build() method.
+  @override
+  void initState() {
+    super.initState();
+    widget.item.createDateTime.hour;
+  }
+
+  // Method to return time in 12 hours format
+  int getTime() {
+    if (widget.item.createDateTime.hour > 12) {
+      return widget.item.createDateTime.hour - 12;
+    } else {
+      return widget.item.createDateTime.hour;
+    }
+  }
+  // Method to return AM and PM
+  String getAmPm() {
+    if (widget.item.createDateTime.hour > 12) {
+      return 'PM';
+    } else {
+      return 'AM';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        /// Dynamic Blue Circle
-        Container(
-          height: 50,
-          width: 50,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            border: Border.all(width: 2, color: Colors.blue),
-          ),
-          child: Visibility(
-            visible: widget.index.isEven,
-            child: Icon(Icons.check, size: 25),
+        /// Blue Circle
+        InkWell(
+          splashColor: Colors.transparent,
+          onTap: () {
+            widget.item.isTaskCompleted = !widget.item.isTaskCompleted;
+            setState(() {});
+          },
+          child: Container(
+            height: 50,
+            width: 50,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(width: 2, color: Colors.blue),
+              color: widget.item.isTaskCompleted
+                  ? Colors.blue
+                  : Colors.transparent,
+            ),
+            child: Visibility(
+              visible: widget.item.isTaskCompleted,
+              child: Icon(Icons.check, size: 25),
+            ),
           ),
         ),
         SizedBox(width: 10),
@@ -40,6 +72,7 @@ class _ToDoListItemState extends State<ToDoListItem> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              SizedBox(height: MediaQuery.of(context).size.height * 0.005),
               //Title
               Text(
                 widget.item.title,
@@ -60,20 +93,26 @@ class _ToDoListItemState extends State<ToDoListItem> {
                   // Time
                   Icon(Icons.watch_later_outlined, size: 18),
                   SizedBox(width: 2),
-                  Text('11:30 AM', style: TextStyle(fontSize: 13)),
+                  Text(
+                    '${getTime().toString().padLeft(2, '0')}:${widget.item.createDateTime.minute.toString().padLeft(2, '0')} ${getAmPm()}',
+                    style: TextStyle(fontSize: 13),
+                  ),
                   SizedBox(width: 12),
 
                   // Date
                   Icon(Icons.calendar_today_rounded, size: 16),
                   SizedBox(width: 5),
-                  Text('26/11/2024', style: TextStyle(fontSize: 12)),
+                  Text(
+                    '${widget.item.createDateTime.day}/${widget.item.createDateTime.month}/${widget.item.createDateTime.year}',
+                    style: TextStyle(fontSize: 12),
+                  ),
                   SizedBox(width: 12),
 
                   // Notification
                   InkWell(
                     onTap: () {
                       setState(() {
-                        widget.item.isRemaindered =!widget.item.isRemaindered;
+                        widget.item.isRemaindered = !widget.item.isRemaindered;
                       });
                     },
                     child: Icon(
@@ -90,7 +129,7 @@ class _ToDoListItemState extends State<ToDoListItem> {
                   Icon(Icons.repeat, size: 18),
                 ],
               ),
-              SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.01),
             ],
           ),
         ),
@@ -98,3 +137,15 @@ class _ToDoListItemState extends State<ToDoListItem> {
     );
   }
 }
+
+// // Method to return time in 12 hours format
+// String getHours() {
+//   int hour = widget.item.createDateTime.hour;
+//   int displayHour = hour > 12 ? hour - 12 : hour;
+//   return displayHour.toString().padLeft(2, '0');
+// }
+//
+// // Method to return minutes in two-digit format
+// String getMinutes() {
+//   return widget.item.createDateTime.minute.toString().padLeft(2, '0');
+// }
