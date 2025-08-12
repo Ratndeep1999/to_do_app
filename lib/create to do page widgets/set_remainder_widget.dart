22bfb4b2e42c822e7500afb9ed990e10be659756
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-12 18:48:42 +0530

    set_remainder_widget.dart : icon and text add, both data set pass dynamically

--- lib/create to do page widgets/set_remainder_widget.dart
+++ lib/create to do page widgets/set_remainder_widget.dart
@@ -1,30 +1,44 @@
 import 'package:flutter/material.dart';
 
 class SetRemainderWidget extends StatelessWidget {
-  const SetRemainderWidget({
-    super.key,
-  });
+  final bool isRemainder;
+  final Function(bool) activeRemainder ;
+
+  const SetRemainderWidget({super.key, required this.isRemainder, required this.activeRemainder});
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {},
+      splashFactory: NoSplash.splashFactory, // Disables the splash effect
+      onTap: () {
+        // Toggle the remainder state
+        activeRemainder(!isRemainder);
+      },
       child: Container(
-        height: MediaQuery.of(context).size.height * 0.055,
-        width: MediaQuery.of(context).size.width * 0.45,
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
         decoration: BoxDecoration(
-          border: Border.all(color: Colors.black26, width: 2),
-          borderRadius: BorderRadius.circular(30),
+          color: isRemainder ? Colors.black : Colors.white,
+          border: Border.all(color: Colors.black26, width: 1.5),
+          borderRadius: BorderRadius.circular(40),
         ),
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
           children: [
             Text(
               'Set Reminder',
-              style: TextStyle(fontWeight: FontWeight.bold),
+              style: TextStyle(
+                color: isRemainder ? Colors.white : Colors.black,
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
             ),
             SizedBox(width: MediaQuery.of(context).size.width * 0.01),
-            Icon(Icons.notifications, size: 18),
+
+            Icon(
+              isRemainder ? Icons.notifications_active : Icons.notifications,
+              size: 20,
+              color: isRemainder ? Colors.white : Colors.black, // White when active, black when inactive
+            ),
           ],
         ),
       ),
