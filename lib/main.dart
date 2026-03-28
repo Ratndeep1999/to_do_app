29d611eb88d813fec31c87c8bb27f324b8e4dacd
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-28 14:49:51 +0530

    main.dart: ToDoApp() remove.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:to_do_app/to_do_list_page.dart';
+import 'core/app/to_do_app.dart';
 
 void main() => runApp(ToDoApp());
-
-class ToDoApp extends StatelessWidget {
-  const ToDoApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.light(),
-      home: ToDoListPage(),
-    );
-  }
-}
