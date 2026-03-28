fda9e491f82a980d1f31e3f9c5842fa833c0e968
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-28 15:13:09 +0530

    to_do_app.dart: home: SplashScreen() add.

---
+++ lib/core/app/to_do_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_app/features/splash/screens/splash_screen.dart';
+
+class ToDoApp extends StatelessWidget {
+  const ToDoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      home: SplashScreen(),
+    );
+  }
+}
