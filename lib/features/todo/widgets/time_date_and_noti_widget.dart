a953567d149320a91b5decd202ebe211040f31ba
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-30 21:52:29 +0530

    time_date_and_noti_widget.dart: TimeDateAndNotiWidget() refactor.

---
+++ lib/features/todo/widgets/time_date_and_noti_widget.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class TimeDateAndNotiWidget extends StatelessWidget {
+  const TimeDateAndNotiWidget({
+    super.key,
+    required this.time,
+    required this.date,
+    required this.isRemainder,
+    required this.onTapRemainder,
+  });
+
+  final String time;
+  final String date;
+  final bool isRemainder;
+  final VoidCallback onTapRemainder;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        /// Time
+        const Icon(Icons.watch_later_outlined, size: 18),
+        const SizedBox(width: 2),
+        Text(time, style: TextStyle(fontSize: 13)),
+        const SizedBox(width: 12),
+
+        /// Date
+        const Icon(Icons.calendar_today_rounded, size: 16),
+        const SizedBox(width: 5),
+        Text(date, style: TextStyle(fontSize: 12)),
+        const SizedBox(width: 12),
+
+        /// Notification
+        InkWell(
+          onTap: onTapRemainder,
+          child: Icon(
+            isRemainder
+                ? Icons.notifications_active_sharp
+                : Icons.notifications,
+            size: 18,
+          ),
+        ),
+        const SizedBox(width: 12),
+
+        /// Refresh
+        const Icon(Icons.repeat, size: 18),
+      ],
+    );
+  }
+}
+
+// '${getTime().toString().padLeft(2, '0')}:${widget.item.createDateTime.minute.toString().padLeft(2, '0')} ${getAmPm()}',
+// '${widget.item.createDateTime.day}/${widget.item.createDateTime.month}/${widget.item.createDateTime.year}',
