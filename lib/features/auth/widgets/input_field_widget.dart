d2b56281e017a6db6b16b6053c5109a964e52303
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-30 14:37:37 +0530

    input_field_widget.dart: enabledBorder, focusedBorder, errorBorder, focusedErrorBorder, textCapitalization, keyboardType,and errorStyle add.

--- lib/features/auth/widgets/input_field_widget.dart
+++ lib/features/auth/widgets/input_field_widget.dart
@@ -29,15 +29,47 @@ class InputFieldWidget extends StatelessWidget {
       validator: validator,
       onChanged: onChanged,
       obscureText: isObscure,
+      textCapitalization: TextCapitalization.characters,
+      keyboardType: TextInputType.text,
+      // onSaved: ,
+      // focusNode: ,
+      // onFieldSubmitted: ,
+      enableSuggestions: true,
       decoration: InputDecoration(
+        contentPadding: EdgeInsets.symmetric(vertical: 14.0),
         hintText: hintText,
         hintStyle: AppTexts.kHintTextStyle,
+        errorStyle: TextStyle(color: Colors.black54, fontSize: 10),
         prefixIcon: Icon(prefix),
         prefixIconColor: Colors.black26,
-        suffixIcon: InkWell(onTap: onSuffixTap, child: Icon(suffix)),
+        suffixIcon: InkWell(
+          onTap: onSuffixTap,
+          highlightColor: Colors.transparent,
+          splashColor: Colors.transparent,
+          child: Icon(suffix, color: Colors.black26),
+        ),
         filled: true,
         fillColor: Colors.grey.shade200,
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(50),
+          borderSide: BorderSide.none,
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(50),
+          borderSide: BorderSide.none,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(50),
+          borderSide: BorderSide.none,
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(50),
+          borderSide: BorderSide.none,
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(50),
+          borderSide: BorderSide.none,
+        ),
       ),
     );
   }
