d3d80cbee305458a49b99b942225eb3208cdbecd
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-30 21:59:28 +0530

    todo_list_items_widget.dart: DescriptionWidget() and  TimeDateAndNotiWidget() add.

--- lib/features/todo/widgets/todo_list_items_widget.dart
+++ lib/features/todo/widgets/todo_list_items_widget.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_app/features/todo/widgets/description_widget.dart';
+import 'package:to_do_app/features/todo/widgets/time_date_and_noti_widget.dart';
+import 'package:to_do_app/features/todo/widgets/title_widget.dart';
 import 'package:to_do_app/features/todo/widgets/toggle_circle_widget.dart';
 
 class TodoListItemsWidget extends StatelessWidget {
@@ -11,6 +14,32 @@ class TodoListItemsWidget extends StatelessWidget {
         /// Toggle Circle Widget
         ToggleCircleWidget(isSelected: false, onTap: () {}),
         SizedBox(width: 10),
+
+        /// ToDoData
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: MediaQuery.of(context).size.height * 0.005),
+
+              /// Title Widget
+              TitleWidget(title: "This is the title"),
+
+              /// Description Widget
+              DescriptionWidget(desc: "This is the description in depth"),
+              SizedBox(height: 2),
+
+              /// Time, Date, Notification, Refresh
+              TimeDateAndNotiWidget(
+                time: "08:25 AM",
+                date: "03/02/1999",
+                isRemainder: false,
+                onTapRemainder: () {},
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+            ],
+          ),
+        ),
       ],
     );
   }
