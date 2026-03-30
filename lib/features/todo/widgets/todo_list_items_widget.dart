3f568f6a8a61da04e1caccf4dd2341f661779816
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-30 22:06:55 +0530

    todo_list_items_widget.dart: final bool isTodoSelected, isRemainder; -- final VoidCallback onTapToggle, onTapRemainder; -- final String title, description, time, date; parameters add.

--- lib/features/todo/widgets/todo_list_items_widget.dart
+++ lib/features/todo/widgets/todo_list_items_widget.dart
@@ -5,14 +5,28 @@ import 'package:to_do_app/features/todo/widgets/title_widget.dart';
 import 'package:to_do_app/features/todo/widgets/toggle_circle_widget.dart';
 
 class TodoListItemsWidget extends StatelessWidget {
-  const TodoListItemsWidget({super.key});
+  const TodoListItemsWidget({
+    super.key,
+    required this.isTodoSelected,
+    required this.isRemainder,
+    required this.onTapToggle,
+    required this.onTapRemainder,
+    required this.title,
+    required this.description,
+    required this.time,
+    required this.date,
+  });
+
+  final bool isTodoSelected, isRemainder;
+  final VoidCallback onTapToggle, onTapRemainder;
+  final String title, description, time, date;
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
         /// Toggle Circle Widget
-        ToggleCircleWidget(isSelected: false, onTap: () {}),
+        ToggleCircleWidget(isSelected: isTodoSelected, onTap: onTapToggle),
         SizedBox(width: 10),
 
         /// ToDoData
@@ -23,18 +37,18 @@ class TodoListItemsWidget extends StatelessWidget {
               SizedBox(height: MediaQuery.of(context).size.height * 0.005),
 
               /// Title Widget
-              TitleWidget(title: "This is the title"),
+              TitleWidget(title: title),
 
               /// Description Widget
-              DescriptionWidget(desc: "This is the description in depth"),
+              DescriptionWidget(desc: description),
               SizedBox(height: 2),
 
               /// Time, Date, Notification, Refresh
               TimeDateAndNotiWidget(
-                time: "08:25 AM",
-                date: "03/02/1999",
-                isRemainder: false,
-                onTapRemainder: () {},
+                time: time,
+                date: date,
+                isRemainder: isRemainder,
+                onTapRemainder: onTapRemainder,
               ),
               SizedBox(height: MediaQuery.of(context).size.height * 0.01),
             ],
