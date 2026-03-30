abfc432808ea2527ee559af93e8383bb7ffd126b
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-30 23:13:45 +0530

    home_screen.dart: CreateTodoScreen() with library add.

--- lib/features/todo/screens/home/home_screen.dart
+++ lib/features/todo/screens/home/home_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_app/core/utils/constants/app_text_styles.dart';
+import 'package:to_do_app/features/todo/screens/create_todo/create_todo_screen.dart';
 import 'package:to_do_app/features/todo/screens/home/widgets/floating_action_button_widget.dart';
 import 'package:to_do_app/features/todo/screens/home/widgets/today_and_filter_widget.dart';
 import 'package:to_do_app/features/todo/screens/home/widgets/todo_list_widget.dart';
 import 'package:to_do_app/to_do_model_class.dart';
-import '../../../../create_to_do_list_page.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -56,7 +56,7 @@ class _HomeScreenState extends State<HomeScreen> {
     ToDoModel? data =
         await Navigator.push(
               context,
-              MaterialPageRoute(builder: (context) => CreateToDoListPage()),
+              MaterialPageRoute(builder: (context) => CreateTodoScreen()),
             )
             as ToDoModel?;
 
