a0495a80edb942dd9db09c436adb302967bbf8c8
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-30 16:45:52 +0530

    text_button_widget.dart: label parameter pass.

--- lib/features/auth/widgets/text_button_widget.dart
+++ lib/features/auth/widgets/text_button_widget.dart
@@ -2,15 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:to_do_app/features/auth/widgets/label_widget.dart';
 
 class TextButtonWidget extends StatelessWidget {
-  const TextButtonWidget({super.key, required this.onTap});
+  const TextButtonWidget({super.key, required this.onTap, required this.label});
 
   final VoidCallback onTap;
+  final String label;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: onTap,
-      child: LabelWidget(label: "SIGN up"),
+      child: LabelWidget(label: label),
     );
   }
 }
