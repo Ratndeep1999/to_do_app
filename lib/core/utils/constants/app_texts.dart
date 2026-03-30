2f9c00f0e1b4a3892911ba3d75bd5be317445cbb
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-30 13:40:35 +0530

    app_texts.dart: kAuthButtonLabel TextStyle add.

---
+++ lib/core/utils/constants/app_texts.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppTexts {
+  AppTexts._();
+
+  static TextStyle kAuthButtonLabel = TextStyle(
+    color: Colors.white,
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+  );
+}
