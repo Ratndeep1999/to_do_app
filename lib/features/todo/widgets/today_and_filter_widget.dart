b18c7fcc31327ae2045842a81f94fafc96307944
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-30 19:47:40 +0530

    today_and_filter_widget.dart: TodayAndFilterWidget() refactor.

---
+++ lib/features/todo/widgets/today_and_filter_widget.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_app/core/utils/constants/app_text_styles.dart';
+
+class TodayAndFilterWidget extends StatelessWidget {
+  const TodayAndFilterWidget({super.key, required this.onTap});
+
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        /// Today
+        Text('Today', style: AppTextStyles.kTodayLabel),
+        Spacer(),
+
+        /// Filter Button
+        InkWell(
+          onTap: onTap,
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+          child: SizedBox(
+            height: 40,
+            width: 100,
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                border: BoxBorder.all(width: 2, color: Colors.black12),
+                borderRadius: BorderRadius.circular(50),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  /// Label
+                  Text(
+                    'Filter ',
+                    style: AppTextStyles.kTodayLabel.copyWith(fontSize: 16),
+                  ),
+
+                  /// Label
+                  const Icon(Icons.filter_alt, size: 20),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
