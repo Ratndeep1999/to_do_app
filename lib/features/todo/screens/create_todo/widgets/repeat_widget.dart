6c5192c3bb97ede71e3ae1443185213f13980600
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-31 23:10:35 +0530

    repeat_widget.dart: RepeatWidget() refactor.

--- lib/features/todo/screens/create_todo/widgets/repeat_widget.dart
+++ lib/features/todo/screens/create_todo/widgets/repeat_widget.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'chip_widget.dart';
 
 class RepeatWidget extends StatelessWidget {
-  const RepeatWidget({super.key});
+  const RepeatWidget({
+    super.key,
+    required this.repeatData,
+    required this.selectedChip,
+    required this.onSelect,
+  });
+
+  final List repeatData;
+  final String selectedChip;
+  final Function(String) onSelect;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Wrap(
+      direction: Axis.horizontal,
+      runSpacing: MediaQuery.of(context).size.height * 0.012,
+      spacing: MediaQuery.of(context).size.width * 0.03,
+      children: List.generate(repeatData.length, (int index) {
+        final item = repeatData[index];
+        return ChipWidget(
+          title: item,
+          isSelected: (selectedChip == item),
+          onSelect: onSelect,
+        );
+      }),
+    );
   }
 }
