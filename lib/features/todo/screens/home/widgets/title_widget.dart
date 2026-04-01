8158f59965a5194dbfc7aad35532adff44063a13
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-04-01 15:17:40 +0530

    title_widget.dart: decoration: isTaskComplete ? TextDecoration.lineThrough : TextDecoration.none add

--- lib/features/todo/screens/home/widgets/title_widget.dart
+++ lib/features/todo/screens/home/widgets/title_widget.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
 
 class TitleWidget extends StatelessWidget {
-  const TitleWidget({super.key, required this.title});
+  const TitleWidget({
+    super.key,
+    required this.title,
+    required this.isTaskComplete,
+  });
 
   final String title;
+  final bool isTaskComplete;
 
   @override
   Widget build(BuildContext context) {
     return Text(
       title,
-      style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+      overflow: TextOverflow.ellipsis,
+      style: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w500,
+        decoration: isTaskComplete
+            ? TextDecoration.lineThrough
+            : TextDecoration.none,
+      ),
     );
   }
 }
