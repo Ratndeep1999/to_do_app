87a5b65913adbce8c624e0d35f5970e986b3a164
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-04-01 15:05:49 +0530

    home_screen.dart: replace ToDoModel with TodoModel.

--- lib/features/todo/screens/home/home_screen.dart
+++ lib/features/todo/screens/home/home_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_app/core/utils/constants/app_text_styles.dart';
+import 'package:to_do_app/features/todo/model/todo_model.dart';
 import 'package:to_do_app/features/todo/screens/create_todo/create_todo_screen.dart';
 import 'package:to_do_app/features/todo/screens/home/widgets/floating_action_button_widget.dart';
 import 'package:to_do_app/features/todo/screens/home/widgets/today_and_filter_widget.dart';
 import 'package:to_do_app/features/todo/screens/home/widgets/todo_list_widget.dart';
-import 'package:to_do_app/to_do_model_class.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -14,8 +14,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  ///
-  List<ToDoModel> toDoList = [];
+  List<TodoModel> toDoList = [];
 
   @override
   Widget build(BuildContext context) {
@@ -53,12 +52,12 @@ class _HomeScreenState extends State<HomeScreen> {
 
   ///
   Future<void> onCreateTodo() async {
-    ToDoModel? data =
+    TodoModel? data =
         await Navigator.push(
               context,
               MaterialPageRoute(builder: (context) => CreateTodoScreen()),
             )
-            as ToDoModel?;
+            as TodoModel?;
 
     if (data != null) {
       setState(() => toDoList.add(data));
