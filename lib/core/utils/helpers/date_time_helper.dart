34ac4af1bed75b06fe40d72d49a1a83a9d825fab
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-04-01 14:40:25 +0530

    date_time_helper.dart: formatTime() add.

--- lib/core/utils/helpers/date_time_helper.dart
+++ lib/core/utils/helpers/date_time_helper.dart
@@ -1,4 +1,14 @@
 class DateTimeHelper {
   /// Private Constructor
   DateTimeHelper._();
+
+  /// Formated Time
+  static String formatTime(DateTime dateTime) {
+    final hour = dateTime.hour % 12 == 0 ? 12 : dateTime.hour % 12;
+    final minute = dateTime.minute.toString().padLeft(2, "0");
+    final period = dateTime.hour >= 12 ? "PM" : "AM";
+    return "$hour : $minute $period";
+  }
+
+  /// Formated Date
 }
