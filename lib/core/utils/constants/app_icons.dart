95119efb4859e3ef8e2e66dd1d6df751325e81a7
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-04-02 09:59:58 +0530

    app_icons.dart: AppIcons() - Onboarding icons path add.

---
+++ lib/core/utils/constants/app_icons.dart
@@ -0,0 +1,9 @@
+class AppIcons {
+  AppIcons._();
+
+  /// Onboarding Icons Path
+  static const String onboardingIcon1 = "assets/onboarding/title1.png";
+  static const String onboardingIcon2 = "assets/onboarding/title2.png";
+  static const String onboardingIcon3 = "assets/onboarding/title3.png";
+  static const String onboardingIcon4 = "assets/onboarding/title4.png";
+}
