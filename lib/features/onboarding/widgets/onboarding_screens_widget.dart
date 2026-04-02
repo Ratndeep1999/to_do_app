9575a9ec78b731716d0a23f966d5797815e43e74
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-04-02 14:22:04 +0530

    onboarding_screen.dart: DotIndicatorWidget() refactor.

---
+++ lib/features/onboarding/widgets/onboarding_screens_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OnboardingScreensWidget extends StatelessWidget {
+  const OnboardingScreensWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
