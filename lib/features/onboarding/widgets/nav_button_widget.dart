27b7cf15b52538a8a08dd4d314c18a035fbe3dce
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-04-02 20:50:52 +0530

    nav_button_widget.dart: refactored NAvButtonWidget add.

---
+++ lib/features/onboarding/widgets/nav_button_widget.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class NavButtonWidget extends StatelessWidget {
+  const NavButtonWidget({super.key, required this.onTap, this.isLeft});
+
+  final VoidCallback onTap;
+  final bool? isLeft;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      splashColor: Colors.white,
+      child: SizedBox(
+        height: 50,
+        width: 50,
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            color: Colors.black45,
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          child: Transform.rotate(
+            angle: (isLeft ?? false) ? 1 : 0,
+            child: Icon(Icons.play_arrow, size: 30),
+          ),
+        ),
+      ),
+    );
+  }
+}
