751aca366d64b2cb67135e96b2abf068fd540f7e
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-04-02 09:32:43 +0530

    app_strings.dart; AppStrings() - Onboarding titles and descriptions add.

---
+++ lib/core/utils/constants/app_strings.dart
@@ -0,0 +1,21 @@
+class AppStrings {
+  AppStrings._();
+
+  /// Onboarding Data
+
+  // Onboarding Titles
+  static const String kOnboardingTitle1 = "Stay Organized Effortlessly";
+  static const String kOnboardingTitle2 = "Plan Your Day Better";
+  static const String kOnboardingTitle3 = "Never Miss a Task";
+  static const String kOnboardingTitle4 = "Get Started Now";
+
+  // Onboarding Description
+  static const String kOnboardingDesc1 =
+      "Manage your daily tasks in one place and never miss what matters most.";
+  static const String kOnboardingDesc2 =
+      "Create, edit, and track tasks easily to keep your day productive and stress-free.";
+  static const String kOnboardingDesc3 =
+      "Set reminders and stay on top of your important activities every day.";
+  static const String kOnboardingDesc4 =
+      "Take control of your tasks and boost your productivity with ease.";
+}
