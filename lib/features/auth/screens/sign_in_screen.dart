459fe28ce1101928fab3b0c9e69b1fd3ab0a367a
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-04-03 17:18:44 +0530

    sign_in_screen.dart: Refactored SignInFormWidget() add.

--- lib/features/auth/screens/sign_in_screen.dart
+++ lib/features/auth/screens/sign_in_screen.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:to_do_app/core/utils/validators/auth_validators.dart';
 import 'package:to_do_app/core/widgets/unfocus_keyboard_widget.dart';
 import 'package:to_do_app/features/auth/screens/sign_up_screen.dart';
-import 'package:to_do_app/features/auth/widgets/input_field_widget.dart';
-import 'package:to_do_app/features/auth/widgets/button_widget.dart';
-import 'package:to_do_app/features/auth/widgets/label_widget.dart';
-import 'package:to_do_app/features/auth/widgets/sub_label_widget.dart';
-import 'package:to_do_app/features/auth/widgets/text_button_widget.dart';
+import 'package:to_do_app/features/auth/widgets/sign_in_form_widget.dart';
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
@@ -44,56 +39,16 @@ class _SignInScreenState extends State<SignInScreen> {
         child: SingleChildScrollView(
           child: SafeArea(
             minimum: EdgeInsets.symmetric(vertical: 80.0),
-            child: Form(
-              key: _formKey,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 50),
-                child: Column(
-                  children: [
-                    /// Sign in LabelWidget
-                    LabelWidget(label: 'SIGN in'),
-                    SizedBox(height: height * 0.025),
-
-                    /// Username InputFieldWidget
-                    InputFieldWidget(
-                      controller: _userNameController,
-                      validator: AuthValidators.userName,
-                      hintText: 'Enter your user name',
-                      prefix: Icons.person,
-                    ),
-                    SizedBox(height: height * 0.025),
-
-                    /// Password InputFieldWidget
-                    InputFieldWidget(
-                      controller: _passwordController,
-                      validator: AuthValidators.password,
-                      onChanged: (val) => debugPrint("Pass : $val"),
-                      hintText: "Enter your password",
-                      isObscure: _isPassVisible,
-                      prefix: Icons.password,
-                      suffix: _isPassVisible
-                          ? Icons.visibility
-                          : Icons.visibility_off,
-                      onSuffixTap: () =>
-                          setState(() => _isPassVisible = !_isPassVisible),
-                    ),
-                    SizedBox(height: height * 0.025),
-
-                    /// Sign In Button To Validate
-                    ButtonWidget(
-                      onButtonPress: onSignInPress,
-                      label: "Sign In",
-                    ),
-                    SizedBox(height: height * 0.075),
-
-                    /// SubLabelWidget
-                    SubLabelWidget(label: 'If you are new here then click on'),
-
-                    /// Sign up TextButtonWidget
-                    TextButtonWidget(onTap: authNavigation, label: "Sign up"),
-                  ],
-                ),
-              ),
+            child: SignInFormWidget(
+              formKey: _formKey,
+              userNameController: _userNameController,
+              passwordController: _passwordController,
+              isPassVisible: _isPassVisible,
+              onTogglePassword: () =>
+                  setState(() => _isPassVisible = !_isPassVisible),
+              onSignInPress: onSignInPress,
+              onNavigate: authNavigation,
+              onPassChanged: (pass) => debugPrint("Password: $pass"),
             ),
           ),
         ),
