7d2c45b7cd63c5552499202e4fe2007fe80f0d37
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-04-03 14:12:59 +0530

    shared_pref_service.dart: object create - static SharedPreferences? _prefs, instance - get _instancePrefs, keys, setOnboardingStatus() and getOnboardingStatus() add.

--- lib/core/services/shared_pref_service.dart
+++ lib/core/services/shared_pref_service.dart
@@ -1,3 +1,5 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
 class SharedPrefService {
   /// Singleton
   static final SharedPrefService _instance = SharedPrefService.internal();
@@ -5,4 +7,26 @@ class SharedPrefService {
   SharedPrefService.internal();
 
   factory SharedPrefService() => _instance;
+
+  static SharedPreferences? _prefs;
+
+  /// Get Instance
+  Future<SharedPreferences> get _instancePrefs async {
+    return _prefs ??= await SharedPreferences.getInstance();
+  }
+
+  /// Keys
+  static const String kOnboardingStatus = "onboarding_status";
+  static const String kSignInStatus = "sign_in_status";
+
+  /// Onboarding Staus Methods
+  Future<void> setOnboardingStatus() async {
+    final prefs = await _instancePrefs;
+    prefs.setBool(kOnboardingStatus, true);
+  }
+
+  Future<bool> getOnboardingStatus() async {
+    final prefs = await _instancePrefs;
+    return prefs.getBool(kOnboardingStatus) ?? false;
+  }
 }
