8f41d36876330c96b0555a38241a22b90d38226d
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-04-03 20:50:28 +0530

    username_field_widget.dart: UsernameFieldWidget() move to fields directory.

--- lib/features/auth/widgets/fields/username_field_widget.dart
+++ lib/features/auth/widgets/fields/username_field_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_app/core/utils/validators/auth_validators.dart';
-import 'Input_field_widget.dart';
+import 'package:to_do_app/features/auth/widgets/Input_field_widget.dart';
 
 class UsernameFieldWidget extends StatelessWidget {
   const UsernameFieldWidget({super.key, required this.controller, this.icon});
