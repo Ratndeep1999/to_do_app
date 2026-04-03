117ffb194a03991433a4f4f4ec6a566974527b75
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-04-03 21:40:43 +0530

    sign_up_screen.dart: SignUpFormWidget() and dispose() add.

--- lib/features/auth/screens/sign_up_screen.dart
+++ lib/features/auth/screens/sign_up_screen.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_app/core/widgets/unfocus_keyboard_widget.dart';
 import 'package:to_do_app/features/auth/screens/sign_in_screen.dart';
-import 'package:to_do_app/features/auth/widgets/button_widgets/button_widget.dart';
-import 'package:to_do_app/features/auth/widgets/field_widgets/email_field_widget.dart';
-import 'package:to_do_app/features/auth/widgets/field_widgets/full_name_field_widget.dart';
-import 'package:to_do_app/features/auth/widgets/field_widgets/password_field_widget.dart';
-import 'package:to_do_app/features/auth/widgets/field_widgets/phone_no_field_widget.dart';
-import 'package:to_do_app/features/auth/widgets/field_widgets/username_field_widget.dart';
-import 'package:to_do_app/features/auth/widgets/label_widgets/sub_label_widget.dart';
-import 'package:to_do_app/features/auth/widgets/button_widgets/text_button_widget.dart';
+import 'package:to_do_app/features/auth/widgets/form_widgets/sign_up_form_widget.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -24,10 +17,20 @@ class _SignUpScreenState extends State<SignUpScreen> {
   final _emailCont = TextEditingController();
   final _mobileNoCont = TextEditingController();
   final _passwordCont = TextEditingController();
+  bool _isPassVis = true;
+
+  @override
+  void dispose() {
+    _fullnameCont.dispose();
+    _usernameCont.dispose();
+    _emailCont.dispose();
+    _mobileNoCont.dispose();
+    _passwordCont.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final height = MediaQuery.of(context).size.height;
     return Scaffold(
       /// Appbar
       appBar: AppBar(
@@ -41,57 +44,17 @@ class _SignUpScreenState extends State<SignUpScreen> {
         child: UnfocusKeyboardWidget(
           child: SafeArea(
             minimum: EdgeInsets.symmetric(vertical: 30.0),
-            child: Form(
-              key: _formKey,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 50),
-                child: Column(
-                  children: [
-                    /// FullName
-                    FullNameFieldWidget(controller: _fullnameCont),
-                    SizedBox(height: height * 0.025),
-
-                    /// Username
-                    UsernameFieldWidget(
-                      controller: _usernameCont,
-                      icon: Icons.verified_user,
-                    ),
-                    SizedBox(height: height * 0.025),
-
-                    /// Email
-                    EmailFieldWidget(controller: _emailCont),
-                    SizedBox(height: height * 0.025),
-
-                    /// Phone no.
-                    PhoneNoFieldWidget(controller: _mobileNoCont),
-                    SizedBox(height: height * 0.025),
-
-                    /// Password
-                    PasswordFieldWidget(
-                      controller: _passwordCont,
-                      icon: Icons.password,
-                      isPassVisible: false,
-                      onToggle: () {},
-                    ),
-                    SizedBox(height: height * 0.025),
-
-                    /// SignUp Button
-                    ButtonWidget(
-                      onButtonPress: onSignUpPress,
-                      label: "Sign Up",
-                    ),
-                    SizedBox(height: height * 0.075),
-
-                    /// Sub Label
-                    SubLabelWidget(
-                      label: 'If you already have account then click on',
-                    ),
-
-                    /// SignUp Label
-                    TextButtonWidget(onTap: authNavigation, label: "Sign in"),
-                  ],
-                ),
-              ),
+            child: SignUpFormWidget(
+              formKey: _formKey,
+              fullnameCont: _fullnameCont,
+              usernameCont: _usernameCont,
+              emailCont: _emailCont,
+              mobileNoCont: _mobileNoCont,
+              passwordCont: _passwordCont,
+              isPassVisible: _isPassVis,
+              onTogglePassword: () => setState(() => _isPassVis = !_isPassVis),
+              onSignUpPress: onSignUpPress,
+              onNavigate: authNavigation,
             ),
           ),
         ),
