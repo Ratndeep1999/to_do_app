a74e6bc1302bbcf132b86deeb6d64e9625571bcc
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-04-03 20:35:16 +0530

    full_name_field_widget.dart: FullNameFieldWidget() refactor.

---
+++ lib/features/auth/widgets/full_name_field_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_app/core/utils/validators/auth_validators.dart';
+
+import 'Input_field_widget.dart';
+
+class FullNameFieldWidget extends StatelessWidget {
+  const FullNameFieldWidget({super.key, required this.controller});
+
+  final TextEditingController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return InputFieldWidget(
+      controller: controller,
+      hintText: "Enter your full name",
+      validator: AuthValidators.fullName,
+      icon: Icons.person,
+    );
+  }
+}
