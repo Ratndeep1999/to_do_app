10a3b6b1876b3cc7d4c39fa49732cc0ccc7a6ba4
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-04-03 20:38:44 +0530

    username_field_widget.dart: icon parameter add.

--- lib/features/auth/widgets/username_field_widget.dart
+++ lib/features/auth/widgets/username_field_widget.dart
@@ -3,9 +3,10 @@ import 'package:to_do_app/core/utils/validators/auth_validators.dart';
 import 'Input_field_widget.dart';
 
 class UsernameFieldWidget extends StatelessWidget {
-  const UsernameFieldWidget({super.key, required this.controller});
+  const UsernameFieldWidget({super.key, required this.controller, this.icon});
 
   final TextEditingController controller;
+  final IconData? icon;
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +15,7 @@ class UsernameFieldWidget extends StatelessWidget {
       validator: AuthValidators.userName,
       hintText: 'Enter your user name',
       prefix: Icons.person,
+      icon: icon,
     );
   }
 }
