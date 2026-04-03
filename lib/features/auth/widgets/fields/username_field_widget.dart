9045232bb376fbbc5eff8b6544fbf021a75d3535
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-04-03 21:03:10 +0530

    username_field_widget.dart: prefix parameter add.

--- lib/features/auth/widgets/fields/username_field_widget.dart
+++ lib/features/auth/widgets/fields/username_field_widget.dart
@@ -3,10 +3,16 @@ import 'package:to_do_app/core/utils/validators/auth_validators.dart';
 import 'package:to_do_app/features/auth/widgets/Input_field_widget.dart';
 
 class UsernameFieldWidget extends StatelessWidget {
-  const UsernameFieldWidget({super.key, required this.controller, this.icon});
+  const UsernameFieldWidget({
+    super.key,
+    required this.controller,
+    this.icon,
+    this.prefix,
+  });
 
   final TextEditingController controller;
   final IconData? icon;
+  final IconData? prefix;
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +21,7 @@ class UsernameFieldWidget extends StatelessWidget {
       validator: AuthValidators.userName,
       hintText: 'Enter your user name',
       icon: icon,
+      prefix: prefix,
     );
   }
 }
