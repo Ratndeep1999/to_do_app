4e9ad9eaba44ff1d956b7187430dd90ac3abeab9
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-04-03 21:15:56 +0530

    TextButtonWidget() and ButtonWidget() move to button_widgets directory.

--- lib/features/auth/screens/sign_up_screen.dart
+++ lib/features/auth/screens/sign_up_screen.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_app/core/widgets/unfocus_keyboard_widget.dart';
 import 'package:to_do_app/features/auth/screens/sign_in_screen.dart';
-import 'package:to_do_app/features/auth/widgets/button_widget.dart';
+import 'package:to_do_app/features/auth/widgets/button_widgets/button_widget.dart';
 import 'package:to_do_app/features/auth/widgets/field_widgets/email_field_widget.dart';
 import 'package:to_do_app/features/auth/widgets/field_widgets/full_name_field_widget.dart';
 import 'package:to_do_app/features/auth/widgets/field_widgets/password_field_widget.dart';
 import 'package:to_do_app/features/auth/widgets/field_widgets/phone_no_field_widget.dart';
 import 'package:to_do_app/features/auth/widgets/field_widgets/username_field_widget.dart';
 import 'package:to_do_app/features/auth/widgets/sub_label_widget.dart';
-import 'package:to_do_app/features/auth/widgets/text_button_widget.dart';
+import 'package:to_do_app/features/auth/widgets/button_widgets/text_button_widget.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
