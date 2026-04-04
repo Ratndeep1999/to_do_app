53fa89d7a972f7922c062c2241bcad91d3b47df2
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-04-04 15:14:32 +0530

    user_model.dart: UserModel() add.

---
+++ lib/features/auth/model/user_model.dart
@@ -0,0 +1,15 @@
+class UserModel {
+  final String fullname;
+  final String username;
+  final String email;
+  final String phoneNo;
+  final String password;
+
+  UserModel({
+    required this.fullname,
+    required this.username,
+    required this.email,
+    required this.phoneNo,
+    required this.password,
+  });
+}
