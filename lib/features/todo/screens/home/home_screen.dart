8e3554d6783e4f66e3c5768b91a58c32528d8981
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-04-04 18:33:21 +0530

    home_screen.dart: replace CreateTodoScreen() with TodoFormScreen()

--- lib/features/todo/screens/home/home_screen.dart
+++ lib/features/todo/screens/home/home_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_app/core/utils/constants/app_text_styles.dart';
 import 'package:to_do_app/features/todo/model/todo_model.dart';
-import 'package:to_do_app/features/todo/screens/create_todo/todo_form_screen.dart';
 import 'package:to_do_app/features/todo/screens/home/widgets/floating_action_button_widget.dart';
 import 'package:to_do_app/features/todo/screens/home/widgets/today_and_filter_widget.dart';
 import 'package:to_do_app/features/todo/screens/home/widgets/todo_list_widget.dart';
+import 'package:to_do_app/features/todo/screens/todo_form/todo_form_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -55,7 +55,7 @@ class _HomeScreenState extends State<HomeScreen> {
     TodoModel? data =
         await Navigator.push(
               context,
-              MaterialPageRoute(builder: (context) => CreateTodoScreen()),
+              MaterialPageRoute(builder: (context) => TodoFormScreen()),
             )
             as TodoModel?;
 
