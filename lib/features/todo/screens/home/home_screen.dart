a845caa833acd6ec04ad4648ef8d7e40aa2f85f2
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-04-06 16:20:58 +0530

    home_screen.dart: toDoList change to todoList, .

--- lib/features/todo/screens/home/home_screen.dart
+++ lib/features/todo/screens/home/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_app/core/utils/constants/app_text_styles.dart';
 import 'package:to_do_app/features/todo/model/todo_model.dart';
-import 'package:to_do_app/features/todo/screens/home/widgets/floating_action_button_widget.dart';
+import 'package:to_do_app/features/todo/screens/home/widgets/add_todo_button.dart';
 import 'package:to_do_app/features/todo/screens/home/widgets/header_filter_widget.dart';
 import 'package:to_do_app/features/todo/screens/home/widgets/todo_list_widget.dart';
 import 'package:to_do_app/features/todo/screens/todo_form/todo_form_screen.dart';
@@ -14,7 +14,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<TodoModel> toDoList = [];
+  List<TodoModel> todoList = [];
 
   @override
   Widget build(BuildContext context) {
