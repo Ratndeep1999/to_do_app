fa40281038e0c13c41412b618ea2f2ce4c2ee8af
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-04-07 21:40:05 +0530

    db_service.dart: db getter add.

--- lib/core/services/database/db_service.dart
+++ lib/core/services/database/db_service.dart
@@ -1,6 +1,17 @@
+import 'package:sqflite/sqflite.dart';
+
 class DbService {
   /// Singleton
   DbService._();
 
   static final DbService instance = DbService._();
+
+  static Database? _database;
+
+  /// Return Database
+  Future<Database?> get db async {
+    if (_database != null) return _database!;
+    _database = await _initDb();
+    return _database;
+  }
 }
