faf537dad7307c6d2f2a5d58690702defce40d8c
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-04-07 11:25:38 +0530

    todo_list_view.dart: TodoListItemTile() add.

--- lib/features/todo/screens/home/widgets/todo_list_view.dart
+++ lib/features/todo/screens/home/widgets/todo_list_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_app/features/todo/model/todo_model.dart';
-import 'package:to_do_app/features/todo/screens/home/widgets/todo_list_item_widget.dart';
+import 'package:to_do_app/features/todo/screens/home/widgets/empty_todo_widget.dart';
+import 'package:to_do_app/features/todo/screens/home/widgets/todo_list_item_tile.dart';
 
 class TodoListView extends StatelessWidget {
   const TodoListView({
@@ -19,14 +20,7 @@ class TodoListView extends StatelessWidget {
   @override
   Widget build(ctx) {
     if (todoList.isEmpty) {
-      return Expanded(
-        child: Center(
-          child: Text(
-            "No tasks yet",
-            style: TextStyle(fontSize: 16, color: Colors.black45),
-          ),
-        ),
-      );
+      return EmptyTodoWidget();
     } else {
       return Expanded(
         child: ListView.separated(
@@ -34,36 +28,13 @@ class TodoListView extends StatelessWidget {
           itemBuilder: (ctx, index) {
             /// Get Singular Item
             final item = todoList[index];
-            return Dismissible(
-              key: ValueKey(item.createdAt),
-              direction: DismissDirection.endToStart,
-
-              /// Delete Item
-              onDismissed: (_) => onDelete(index),
-              background: Container(
-                alignment: Alignment.centerRight,
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: const Icon(Icons.delete, color: Colors.white),
-              ),
-              child: GestureDetector(
-                /// Edit Item
-                onTap: () => onItemTap(index),
-                onLongPress: () => onItemTap(index),
-                child: TodoListItemWidget(
-                  isTaskComplete: item.isTaskCompleted,
-                  isReminder: item.isReminder,
-                  onTapToggle: () => onToggleComplete(index),
-                  onReminderToggle: () => onToggleReminder(index),
-                  title: item.title,
-                  description: item.description,
-                  time: item.currentTime,
-                  date: item.currentDate,
-                ),
-              ),
+            return TodoListItemTile(
+              item: item,
+              index: index,
+              onDelete: onDelete,
+              onItemTap: onItemTap,
+              onToggleComplete: onToggleComplete,
+              onToggleReminder: onToggleReminder,
             );
           },
           separatorBuilder: (ctx, index) => Divider(),
