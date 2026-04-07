bc78506ed6d2ecb6a4790754ea5e32cd2cb57a2b
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-04-07 22:04:01 +0530

    todo_local_datasource.dart: insertTodo() add.

--- lib/features/todo/data/todo_local_datasource.dart
+++ lib/features/todo/data/todo_local_datasource.dart
@@ -1,5 +1,13 @@
+import 'package:to_do_app/core/services/database/db_constants.dart';
 import 'package:to_do_app/core/services/database/db_service.dart';
+import 'package:to_do_app/features/todo/model/todo_model.dart';
 
 class TodoLocalDatasource {
   final db = DbService.instance;
+
+  /// Add Todo_ to Database
+  Future<int> insertTodo(TodoModel todo) async {
+    final database = await db.db;
+    return await database!.insert(DbConstants.kTableTodo, todo.toMap());
+  }
 }
