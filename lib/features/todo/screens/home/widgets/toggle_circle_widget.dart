45d183dd1efac42b7101f4e4644d85404072782d
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-04-07 10:29:37 +0530

    toggle_circle_widget.dart: DecoratedBox() remove and AnimatedContainer() add.

--- lib/features/todo/screens/home/widgets/toggle_circle_widget.dart
+++ lib/features/todo/screens/home/widgets/toggle_circle_widget.dart
@@ -15,19 +15,19 @@ class ToggleCircleWidget extends StatelessWidget {
     return InkWell(
       splashColor: Colors.transparent,
       onTap: onTap,
-      child: SizedBox(
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
         height: 50,
         width: 50,
-        child: DecoratedBox(
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            border: Border.all(width: 2, color: Colors.blue),
-            color: isTaskComplete ? Colors.blue : Colors.transparent,
-          ),
-          child: Visibility(
-            visible: isTaskComplete,
-            child: Icon(Icons.check, size: 25),
-          ),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: isTaskComplete ? Colors.black : Colors.transparent,
+          border: Border.all(color: Colors.black, width: 2),
+        ),
+        child: Visibility(
+          visible: isTaskComplete,
+          child: Icon(Icons.check, color: Colors.white, size: 25),
         ),
       ),
     );
