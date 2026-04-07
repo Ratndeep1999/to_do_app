97f0ef4d5312b6cc7fd514a74891a41fc3b7e5b4
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-04-07 10:58:45 +0530

    todo_list_view.dart: condition add to show "No tasks yet".

--- lib/features/todo/screens/home/widgets/todo_list_view.dart
+++ lib/features/todo/screens/home/widgets/todo_list_view.dart
@@ -18,46 +18,57 @@ class TodoListView extends StatelessWidget {
 
   @override
   Widget build(ctx) {
-    return Expanded(
-      child: ListView.separated(
-        itemCount: todoList.length,
-        itemBuilder: (ctx, index) {
-          /// Get Singular Item
-          final item = todoList[index];
-          return Dismissible(
-            key: ValueKey(item.createdAt),
-            direction: DismissDirection.endToStart,
+    if (todoList.isEmpty) {
+      return Expanded(
+        child: Center(
+          child: Text(
+            "No tasks yet",
+            style: TextStyle(fontSize: 16, color: Colors.black45),
+          ),
+        ),
+      );
+    } else {
+      return Expanded(
+        child: ListView.separated(
+          itemCount: todoList.length,
+          itemBuilder: (ctx, index) {
+            /// Get Singular Item
+            final item = todoList[index];
+            return Dismissible(
+              key: ValueKey(item.createdAt),
+              direction: DismissDirection.endToStart,
 
-            /// Delete Item
-            onDismissed: (_) => onDelete(index),
-            background: Container(
-              alignment: Alignment.centerRight,
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              decoration: BoxDecoration(
-                color: Colors.black,
-                borderRadius: BorderRadius.circular(12),
+              /// Delete Item
+              onDismissed: (_) => onDelete(index),
+              background: Container(
+                alignment: Alignment.centerRight,
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: const Icon(Icons.delete, color: Colors.white),
               ),
-              child: const Icon(Icons.delete, color: Colors.white),
-            ),
-            child: GestureDetector(
-              /// Edit Item
-              onTap: () => onItemTap(index),
-              onLongPress: () => onItemTap(index),
-              child: TodoListItemWidget(
-                isTaskComplete: item.isTaskCompleted,
-                isReminder: item.isReminder,
-                onTapToggle: () => onToggleComplete(index),
-                onReminderToggle: () => onToggleReminder(index),
-                title: item.title,
-                description: item.description,
-                time: item.currentTime,
-                date: item.currentDate,
+              child: GestureDetector(
+                /// Edit Item
+                onTap: () => onItemTap(index),
+                onLongPress: () => onItemTap(index),
+                child: TodoListItemWidget(
+                  isTaskComplete: item.isTaskCompleted,
+                  isReminder: item.isReminder,
+                  onTapToggle: () => onToggleComplete(index),
+                  onReminderToggle: () => onToggleReminder(index),
+                  title: item.title,
+                  description: item.description,
+                  time: item.currentTime,
+                  date: item.currentDate,
+                ),
               ),
-            ),
-          );
-        },
-        separatorBuilder: (ctx, index) => Divider(),
-      ),
-    );
+            );
+          },
+          separatorBuilder: (ctx, index) => Divider(),
+        ),
+      );
+    }
   }
 }
