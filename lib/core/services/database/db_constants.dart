5a25f388bcf7dbd85261453e7a962a0bbb5d3b19
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-04-07 21:03:53 +0530

    db_constants.dart: k - constants add.

---
+++ lib/core/services/database/db_constants.dart
@@ -0,0 +1,14 @@
+class DbConstants {
+  static const String kTableTodo = 'todos';
+
+  static const String kColId = 'id';
+  static const String kColTitle = 'title';
+  static const String kColDesc = 'description';
+  static const String kColRepeat = 'repeat';
+  static const String kColDays = 'days';
+  static const String kColReminder = 'isReminder';
+  static const String kColCompleted = 'isTaskCompleted';
+  static const String kColTime = 'currentTime';
+  static const String kColDate = 'currentDate';
+  static const String kColCreatedAt = 'createdAt';
+}
