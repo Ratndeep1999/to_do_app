cea6d6eb8d0a920754cb3bd8541b89daa4bdeccd
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-04-07 15:07:28 +0530

    delete_todo_widget.dart: onDismissed(), onComplete() and secondaryBackground: add.

--- lib/features/todo/screens/home/widgets/delete_todo_widget.dart
+++ lib/features/todo/screens/home/widgets/delete_todo_widget.dart
@@ -6,23 +6,42 @@ class DeleteTodoWidget extends StatelessWidget {
     required this.uniqueKey,
     required this.child,
     required this.onDelete,
+    required this.onComplete,
   });
 
   final Key uniqueKey;
   final Widget child;
-  final VoidCallback onDelete;
+  final VoidCallback onDelete, onComplete;
 
   @override
   Widget build(BuildContext context) {
     return Dismissible(
       key: uniqueKey,
-      direction: DismissDirection.endToStart,
-      onDismissed: (_) => onDelete(),
+      direction: DismissDirection.horizontal,
+      onDismissed: (direction) {
+        if (direction == DismissDirection.endToStart) {
+          onDelete(); // swipe from left
+        } else {
+          onComplete(); // swipe from right
+        }
+      },
+      // Left Side Decoration
       background: Container(
         alignment: Alignment.centerRight,
         padding: const EdgeInsets.symmetric(horizontal: 20),
         decoration: BoxDecoration(
           color: Colors.black,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: const Icon(Icons.delete, color: Colors.white),
+      ),
+
+      // Right Side Decoration
+      secondaryBackground: Container(
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        decoration: BoxDecoration(
+          color: Colors.red,
           borderRadius: BorderRadius.circular(12),
         ),
         child: const Icon(Icons.delete, color: Colors.white),
