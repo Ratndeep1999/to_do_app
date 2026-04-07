7d01958453c8678176a4cd2097702cda73f80372
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-04-07 15:52:58 +0530

    delete_todo_widget.dart: unnecessary code remove.

--- lib/features/todo/screens/home/widgets/delete_todo_widget.dart
+++ lib/features/todo/screens/home/widgets/delete_todo_widget.dart
@@ -17,14 +17,8 @@ class DeleteTodoWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Dismissible(
       key: uniqueKey,
-      direction: DismissDirection.horizontal,
-      onDismissed: (direction) {
-        if (direction == DismissDirection.endToStart) {
-          onDelete(); // swipe from left
-        } else {
-          onComplete(); // swipe from right
-        }
-      },
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) => onDelete(),
       // Left Side Decoration
       background: Container(
         alignment: Alignment.centerRight,
@@ -36,16 +30,6 @@ class DeleteTodoWidget extends StatelessWidget {
         child: const Icon(Icons.delete, color: Colors.white),
       ),
 
-      // Right Side Decoration
-      secondaryBackground: Container(
-        alignment: Alignment.centerRight,
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        decoration: BoxDecoration(
-          color: Colors.red,
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: const Icon(Icons.delete, color: Colors.white),
-      ),
       child: child,
     );
   }
