a3c4772a5dcaec8920a1b9da439d07ff31cafc88
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-04-11 14:45:38 +0530

    app.dart: scaffoldMessengerKey: SnackbarService.instance.messengerKey add.

--- lib/core/app/app.dart
+++ lib/core/app/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_app/core/services/snackbar_service.dart';
 import 'package:to_do_app/features/splash/screens/splash_screen.dart';
 
 class App extends StatelessWidget {
@@ -8,6 +9,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      scaffoldMessengerKey: SnackbarService.instance.messengerKey,
       theme: ThemeData.light(),
       home: SplashScreen(),
     );
