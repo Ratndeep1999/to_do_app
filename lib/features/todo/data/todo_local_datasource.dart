923558639a679211ead45fd4cad9ed9a1f79f3c1
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-04-11 13:58:25 +0530

    todo_local_datasource.dart: getTodos() wrap with try and catch.

--- lib/features/todo/data/todo_local_datasource.dart
+++ lib/features/todo/data/todo_local_datasource.dart
@@ -5,12 +5,12 @@ import 'package:to_do_app/core/services/database/db_service.dart';
 import 'package:to_do_app/features/todo/model/todo_model.dart';
 
 class TodoLocalDatasource {
-  final db = DbService.instance;
+  final dbInstance = DbService.instance;
 
   /// Add Todo_ to Database
   Future<bool> insertTodo(TodoModel todo) async {
     try {
-      final database = await db.db;
+      final database = await dbInstance.db;
       final result = await database.insert(
         DbConstants.kTableTodo,
         todo.toMap(),
@@ -25,19 +25,19 @@ class TodoLocalDatasource {
   /// Get Database Data
   Future<List<TodoModel>> getTodos() async {
     try {
-      final database = await db.db;
+      final database = await dbInstance.db;
       final data = await database.query(DbConstants.kTableTodo);
       return data.map((e) => TodoModel.fromMap(e)).toList();
     } catch (e) {
-      debugPrint("Delete Error: $e");
-      throw DatabaseException("Failed to delete todo");
+      debugPrint("Fetch Error: $e");
+      throw DatabaseException("Failed to fetch todos");
     }
   }
 
   /// Delete_Todo
   Future<bool> deleteTodo(int id) async {
     try {
-      final database = await db.db;
+      final database = await dbInstance.db;
       final result = await database.delete(
         DbConstants.kTableTodo,
         where: "${DbConstants.kColId} = ?",
@@ -52,7 +52,7 @@ class TodoLocalDatasource {
 
   /// Update_Todo
   Future<bool> updateTodo(TodoModel todo) async {
-    final database = await db.db;
+    final database = await dbInstance.db;
     final result = await database.update(
       DbConstants.kTableTodo,
       todo.toMap(),
